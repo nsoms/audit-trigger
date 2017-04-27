5b6defb0294b08cb22d8402682adf058064a85bc
Nikolay Soms <nsoms@users.noreply.github.com>
2017-04-27 11:55:00 +0300

    Removed not needed columns from logged_actions table

--- audit.sql
+++ audit.sql
@@ -44,14 +44,14 @@ CREATE TABLE audit.logged_actions (
     table_name text not null,
     row_id  int,
     relid oid not null,
-    session_user_name text,
-    action_tstamp_tx TIMESTAMP WITH TIME ZONE NOT NULL,
+--     session_user_name text,
+--     action_tstamp_tx TIMESTAMP WITH TIME ZONE NOT NULL,
     action_tstamp_stm TIMESTAMP WITH TIME ZONE NOT NULL,
-    action_tstamp_clk TIMESTAMP WITH TIME ZONE NOT NULL,
-    transaction_id bigint,
-    application_name text,
-    client_addr inet,
-    client_port integer,
+--     action_tstamp_clk TIMESTAMP WITH TIME ZONE NOT NULL,
+--     transaction_id bigint,
+--     application_name text,
+--     client_addr inet,
+--     client_port integer,
     client_query text NOT NULL,
     action TEXT NOT NULL CHECK (action IN ('I','D','U', 'T')),
     row_data hstore,
@@ -65,16 +65,17 @@ COMMENT ON TABLE audit.logged_actions IS 'History of auditable actions on audite
 COMMENT ON COLUMN audit.logged_actions.event_id IS 'Unique identifier for each auditable event';
 COMMENT ON COLUMN audit.logged_actions.schema_name IS 'Database schema audited table for this event is in';
 COMMENT ON COLUMN audit.logged_actions.table_name IS 'Non-schema-qualified table name of table event occured in';
+COMMENT ON COLUMN audit.logged_actions.row_id IS 'Row id field from table.';
 COMMENT ON COLUMN audit.logged_actions.relid IS 'Table OID. Changes with drop/create. Get with ''tablename''::regclass';
-COMMENT ON COLUMN audit.logged_actions.session_user_name IS 'Login / session user whose statement caused the audited event';
-COMMENT ON COLUMN audit.logged_actions.action_tstamp_tx IS 'Transaction start timestamp for tx in which audited event occurred';
+-- COMMENT ON COLUMN audit.logged_actions.session_user_name IS 'Login / session user whose statement caused the audited event';
+-- COMMENT ON COLUMN audit.logged_actions.action_tstamp_tx IS 'Transaction start timestamp for tx in which audited event occurred';
 COMMENT ON COLUMN audit.logged_actions.action_tstamp_stm IS 'Statement start timestamp for tx in which audited event occurred';
-COMMENT ON COLUMN audit.logged_actions.action_tstamp_clk IS 'Wall clock time at which audited event''s trigger call occurred';
-COMMENT ON COLUMN audit.logged_actions.transaction_id IS 'Identifier of transaction that made the change. May wrap, but unique paired with action_tstamp_tx.';
-COMMENT ON COLUMN audit.logged_actions.client_addr IS 'IP address of client that issued query. Null for unix domain socket.';
-COMMENT ON COLUMN audit.logged_actions.client_port IS 'Remote peer IP port address of client that issued query. Undefined for unix socket.';
+-- COMMENT ON COLUMN audit.logged_actions.action_tstamp_clk IS 'Wall clock time at which audited event''s trigger call occurred';
+-- COMMENT ON COLUMN audit.logged_actions.transaction_id IS 'Identifier of transaction that made the change. May wrap, but unique paired with action_tstamp_tx.';
+-- COMMENT ON COLUMN audit.logged_actions.application_name IS 'Application name set when this audit event occurred. Can be changed in-session by client.';
+-- COMMENT ON COLUMN audit.logged_actions.client_addr IS 'IP address of client that issued query. Null for unix domain socket.';
+-- COMMENT ON COLUMN audit.logged_actions.client_port IS 'Remote peer IP port address of client that issued query. Undefined for unix socket.';
 COMMENT ON COLUMN audit.logged_actions.client_query IS 'Top-level query that caused this auditable event. May be more than one statement.';
-COMMENT ON COLUMN audit.logged_actions.application_name IS 'Application name set when this audit event occurred. Can be changed in-session by client.';
 COMMENT ON COLUMN audit.logged_actions.action IS 'Action type; I = insert, D = delete, U = update, T = truncate';
 COMMENT ON COLUMN audit.logged_actions.row_data IS 'Record value. Null for statement-level trigger. For INSERT this is the new tuple. For DELETE and UPDATE it is the old tuple.';
 COMMENT ON COLUMN audit.logged_actions.changed_fields IS 'New values of fields changed by UPDATE. Null except for row-level UPDATE events.';
@@ -83,6 +84,9 @@ COMMENT ON COLUMN audit.logged_actions.statement_only IS '''t'' if audit event i
 CREATE INDEX logged_actions_relid_idx ON audit.logged_actions(relid);
 CREATE INDEX logged_actions_action_tstamp_tx_stm_idx ON audit.logged_actions(action_tstamp_stm);
 CREATE INDEX logged_actions_action_idx ON audit.logged_actions(action);
+
+CREATE INDEX logged_actions_table_row_id_idx ON audit.logged_actions(table_name, row_id);
+CREATE INDEX logged_actions_table_row_id_upd_idx ON audit.logged_actions(table_name, row_id, action_tstamp_stm);
 
 CREATE TABLE audit.logged_relations (
     relation_name text not null,
@@ -115,17 +119,18 @@ BEGIN
         TG_TABLE_NAME::text,                          -- table_name
         NULL,                                         -- row_id !!!
         TG_RELID,                                     -- relation OID for much quicker searches
-        session_user::text,                           -- session_user_name
-        current_timestamp,                            -- action_tstamp_tx
+--         session_user::text,                           -- session_user_name
+--         current_timestamp,                            -- action_tstamp_tx
         statement_timestamp(),                        -- action_tstamp_stm
-        clock_timestamp(),                            -- action_tstamp_clk
-        txid_current(),                               -- transaction ID
-        (SELECT setting FROM pg_settings WHERE name = 'application_name'),
-        inet_client_addr(),                           -- client_addr
-        inet_client_port(),                           -- client_port
+--         clock_timestamp(),                            -- action_tstamp_clk
+--         txid_current(),                               -- transaction ID
+--         (SELECT setting FROM pg_settings WHERE name = 'application_name'),
+--         inet_client_addr(),                           -- client_addr
+--         inet_client_port(),                           -- client_port
         current_query(),                              -- top-level query or queries (if multistatement) from client
         substring(TG_OP,1,1),                         -- action
-        NULL, NULL,                                   -- row_data, changed_fields
+        NULL,                                         -- row_data
+        NULL,                                         -- changed_fields
         'f'                                           -- statement_only
         );
 
